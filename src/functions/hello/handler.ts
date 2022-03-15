248764bf14229c797be7477bcdb0729d9cd314f4
Jesse Chounard <jessechounard@gmail.com>
2022-03-15 13:38:40 -0500

    Added middy schema validation, event/handler types

--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -1,17 +1,35 @@
-import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
-import { formatJSONResponse } from '@libs/api-gateway';
 import middy from '@middy/core';
 import middyJsonBodyParser from '@middy/http-json-body-parser';
+import middyValidator from '@middy/validator';
+import middyHttpErrorHandler from '@middy/http-error-handler';
+import { FromSchema } from 'json-schema-to-ts';
+import { APIGatewayProxyEventWithBodyHandler } from 'src/types/handler';
 
-import schema from './schema';
+const helloSchema = {
+    type: 'object',
+    properties: {
+        name: { type: 'string' },
+    },
+    required: ['name'],
+} as const;
 
-const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
-    event,
-) => {
-    return formatJSONResponse({
+export type HelloData = FromSchema<typeof helloSchema>;
+
+const hello: APIGatewayProxyEventWithBodyHandler<
+    HelloData,
+    Record<string, unknown>
+> = async (event) => {
+    return {
         message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
         event,
-    });
+    };
 };
 
-export const main = middy(hello).use(middyJsonBodyParser());
+export const main = middy(hello)
+    .use(middyJsonBodyParser())
+    .use(
+        middyValidator({
+            inputSchema: { type: 'object', properties: { body: helloSchema } },
+        }),
+    )
+    .use(middyHttpErrorHandler());
---
+++ src/types/handler.ts
@@ -0,0 +1,9 @@
+import { APIGatewayProxyEvent, Handler } from 'aws-lambda';
+
+export type APIGatewayProxyEventWithBody<BodyType> = Omit<
+    APIGatewayProxyEvent,
+    'body'
+> & { body: BodyType };
+
+export type APIGatewayProxyEventWithBodyHandler<BodyType, ResponseType> =
+    Handler<APIGatewayProxyEventWithBody<BodyType>, ResponseType>;
